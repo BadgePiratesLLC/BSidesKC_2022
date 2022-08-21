87bf148bd5b47282c15882c330cf7ef78824b5c9
Patrick Burns <patrickburns@patricks-mbp.lan>
2022-08-21 18:58:46 -0500

    added source code, just the platformio.ini file and a basic main.cpp for now

---
+++ code/src/main.cpp
@@ -0,0 +1,27 @@
+#include <HardwareSerial.h>
+
+
+unsigned int badgeID = 0;
+unsigned int botState = 0;
+unsigned int botLvl = 1;
+unsigned int badgeType = 1;
+
+void setup() {
+  Serial.begin(115200);
+    Serial.println("Setup");
+
+  // Setup Buttons
+//   pinMode(BTNB, INPUT);
+//   pinMode(BTNR, INPUT);
+//   pinMode(BTND, INPUT);
+//   pinMode(BTNC, INPUT);
+//   pinMode(BTNA, INPUT);
+//   pinMode(BTNL, INPUT);
+//   pinMode(BTNU, INPUT);
+
+  
+}
+
+void loop() {
+  
+}
