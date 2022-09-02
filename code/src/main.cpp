5953db9ec58b02a61c65e8f1524d136f25c91672
Patrick Ecord <patrick.ecord@riskiq.net>
2022-09-02 18:02:22 -0500

    clean up, add bling code

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <RotaryEncoder.h>
 
+#define PIN_IN1 13
+#define PIN_IN2 38
+#define ROTARYSTEPS 1
+#define ROTARYMIN 0
+#define ROTARYMAX 11
+
 const int LED_B = 3;
 const int LED_S1 = 34;
 const int LED_I = 4;
@@ -21,26 +27,18 @@ const int LED_9 = 37;
 const int LED_10 = 12;
 const int LED_11 = 17;
 
-// Example for Arduino UNO with input signals on pin 2 and 3
-#define PIN_IN1 13
-#define PIN_IN2 38
+static int pos = 0;
+static int dir = 0;
+static int currentNumber = 0;
+static int lastNumber = 0;
 
-
-#define ROTARYSTEPS 1
-#define ROTARYMIN 0
-#define ROTARYMAX 11
-
-// Setup a RotaryEncoder with 4 steps per latch for the 2 signal input pins:
-// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
+static bool DEBUG=false;
 
 // Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
 RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
 
-// Last known rotary position.
-int lastPos = -1;
-
 void turnOffAllLights() {
-digitalWrite(LED_0, LOW);
+  digitalWrite(LED_0, LOW);
   digitalWrite(LED_1, LOW);
   digitalWrite(LED_2, LOW);
   digitalWrite(LED_3, LOW);
@@ -54,9 +52,8 @@ digitalWrite(LED_0, LOW);
   digitalWrite(LED_11, LOW); 
 }
 
+//TODO make a lookup table for the numeral -> LED_X
 void turnOnNumber(int numeral) {
-    turnOffAllLights();
-
     switch (numeral) {
     case 0:
         digitalWrite(LED_0, HIGH);
@@ -99,16 +96,51 @@ void turnOnNumber(int numeral) {
     }
 }
 
+void turnOffNumber(int numeral) {
+    switch (numeral) {
+    case 0:
+        digitalWrite(LED_0, LOW);
+        break;
+    case 1:
+        digitalWrite(LED_1, LOW);
+        break;
+    case 2:
+        digitalWrite(LED_2, LOW);
+        break;
+    case 3:
+        digitalWrite(LED_3, LOW);
+        break;
+    case 4:
+        digitalWrite(LED_4, LOW);
+        break;
+    case 5:
+        digitalWrite(LED_5, LOW);
+        break;
+    case 6:
+        digitalWrite(LED_6, LOW);
+        break;
+    case 7:
+        digitalWrite(LED_7, LOW);
+        break;
+    case 8:
+        digitalWrite(LED_8, LOW);
+        break;
+    case 9:
+        digitalWrite(LED_9, LOW);
+        break;
+    case 10:
+        digitalWrite(LED_10, LOW);
+        break;
+    case 11:
+        digitalWrite(LED_11, LOW);
+        break;
+    default:
+        break;
+    }
+}
 
-void setup()
-{
-  Serial.begin(115200);
-  while (! Serial);
-  Serial.println("LimitedRotator example for the RotaryEncoder library.");
-  encoder.setPosition(0 / ROTARYSTEPS); 
-
-
-  // Setup LEDS
+// Setup LEDS
+void pullDownAllPins() {
   pinMode(LED_B, OUTPUT); 
   pinMode(LED_S1, OUTPUT); 
   pinMode(LED_I, OUTPUT); 
@@ -127,32 +159,24 @@ void setup()
   pinMode(LED_9, OUTPUT); 
   pinMode(LED_10, OUTPUT); 
   pinMode(LED_11, OUTPUT); 
-
-  turnOffAllLights();
 }
-static int pos = 0;
-static int dir = 0;
-static int currentNumber = 0;
 
-static bool DEBUG=false;
 // Read the current position of the encoder and print out when changed.
-void loop()
-{
+void readEncoder() {
   encoder.tick();
   
   int newPos = encoder.getPosition();
   int newDir = (int)(encoder.getDirection()) * -1;
 
+  // save off the previous number 
+  lastNumber = currentNumber;
+  // get the new number
   currentNumber += newDir;
-
   if (currentNumber > ROTARYMAX) {
     currentNumber = ROTARYMIN;
   } else if (currentNumber < ROTARYMIN) {
     currentNumber = ROTARYMAX;
   }
-
-  turnOffAllLights();
-  turnOnNumber(currentNumber);
 
   if (DEBUG && pos != newPos) {
     Serial.print("pos:");
@@ -161,4 +185,219 @@ void loop()
     Serial.println(newDir);
     pos = newPos;
   }
+}
+
+// TODO copied from BlinkyLights.ino - needs clean up
+void blingMode() {
+    digitalWrite(3,HIGH);
+    delay(25);
+    digitalWrite(3,LOW);
+    delay(25);
+    digitalWrite(34,HIGH);
+    delay(25);
+    digitalWrite(34,LOW);
+    delay(25);
+    digitalWrite(4,HIGH);
+    delay(25);
+    digitalWrite(4,LOW);
+    delay(25);
+    digitalWrite(33,HIGH);
+    delay(25);
+    digitalWrite(33,LOW);
+    delay(25);
+    digitalWrite(5,HIGH);
+    delay(25);
+    digitalWrite(5,LOW);
+    delay(25);
+    digitalWrite(26,HIGH);
+    delay(25);
+    digitalWrite(26,LOW);
+    delay(25);
+
+    digitalWrite(26,HIGH);
+    delay(25);
+    digitalWrite(26,LOW);
+    delay(25);
+    digitalWrite(5,HIGH);
+    delay(25);
+    digitalWrite(5,LOW);
+    delay(25);
+    digitalWrite(33,HIGH);
+    delay(25);
+    digitalWrite(33,LOW);
+    delay(25);
+    digitalWrite(4,HIGH);
+    delay(25);
+    digitalWrite(4,LOW);
+    delay(25);
+    digitalWrite(34,HIGH);
+    delay(25);
+    digitalWrite(34,LOW);
+    delay(25);
+    digitalWrite(3,HIGH);
+    delay(25);
+    digitalWrite(3,LOW);
+    delay(25);
+
+    digitalWrite(6,HIGH);
+    delay(25);
+    digitalWrite(6,LOW);
+    delay(25);
+    digitalWrite(21,HIGH);
+    delay(25);
+    digitalWrite(21,LOW);
+    delay(25);
+    digitalWrite(14,HIGH);
+    delay(25);
+    digitalWrite(14,LOW);
+    delay(25);
+    digitalWrite(35,HIGH);
+    delay(25);
+    digitalWrite(35,LOW);
+    delay(25);
+    digitalWrite(10,HIGH);
+    delay(25);
+    digitalWrite(10,LOW);
+    delay(25);
+    digitalWrite(15,HIGH);
+    delay(25);
+    digitalWrite(15,LOW);
+    delay(25);
+    digitalWrite(36,HIGH);
+    delay(25);
+    digitalWrite(36,LOW);
+    delay(25);
+    digitalWrite(11,HIGH);
+    delay(25);
+    digitalWrite(11,LOW);
+    delay(25);
+    digitalWrite(16,HIGH);
+    delay(25);
+    digitalWrite(16,LOW);
+    delay(25);
+    digitalWrite(37,HIGH);
+    delay(25);
+    digitalWrite(37,LOW);
+    delay(25);
+    digitalWrite(12,HIGH);
+    delay(25);
+    digitalWrite(12,LOW);
+    delay(25);
+    digitalWrite(17,HIGH);
+    delay(25);
+    digitalWrite(17,LOW);
+    delay(25);
+
+
+    digitalWrite(17,HIGH);
+    delay(25);
+    digitalWrite(17,LOW);
+    delay(25);
+    digitalWrite(12,HIGH);
+    delay(25);
+    digitalWrite(12,LOW);
+    delay(25);
+    digitalWrite(37,HIGH);
+    delay(25);
+    digitalWrite(37,LOW);
+    delay(25);
+    digitalWrite(16,HIGH);
+    delay(25);
+    digitalWrite(16,LOW);
+    delay(25);
+    digitalWrite(11,HIGH);
+    delay(25);
+    digitalWrite(11,LOW);
+    delay(25);
+    digitalWrite(36,HIGH);
+    delay(25);
+    digitalWrite(36,LOW);
+    delay(25);
+    digitalWrite(15,HIGH);
+    delay(25);
+    digitalWrite(15,LOW);
+    delay(25);
+    digitalWrite(10,HIGH);
+    delay(25);
+    digitalWrite(10,LOW);
+    delay(25);
+    digitalWrite(35,HIGH);
+    delay(25);
+    digitalWrite(35,LOW);
+    delay(25);
+    digitalWrite(14,HIGH);
+    delay(25);
+    digitalWrite(14,LOW);
+    delay(25);
+    digitalWrite(21,HIGH);
+    delay(25);
+    digitalWrite(21,LOW);
+    delay(25);
+    digitalWrite(6,HIGH);
+    delay(25);
+    digitalWrite(6,LOW);
+    delay(25);
+
+    digitalWrite(3,HIGH);delay(25);
+    digitalWrite(34,HIGH);delay(25);
+    digitalWrite(4,HIGH);delay(25);
+    digitalWrite(33,HIGH);delay(25);
+    digitalWrite(5,HIGH);delay(25);
+    digitalWrite(26,HIGH);delay(1000);
+    digitalWrite(26,LOW);delay(25);
+    digitalWrite(5,LOW);delay(25);
+    digitalWrite(33,LOW);delay(25);
+    digitalWrite(4,LOW);delay(25);
+    digitalWrite(34,LOW);delay(25);
+    digitalWrite(3,LOW);delay(25);
+
+    digitalWrite(6,HIGH);delay(25);
+    digitalWrite(21,HIGH);delay(25);
+    digitalWrite(14,HIGH);delay(25);
+    digitalWrite(35,HIGH);delay(25);
+    digitalWrite(10,HIGH);delay(25);
+    digitalWrite(15,HIGH);delay(25);
+    digitalWrite(36,HIGH);delay(25);
+    digitalWrite(11,HIGH);delay(25);
+    digitalWrite(16,HIGH);delay(25);
+    digitalWrite(37,HIGH);delay(25);
+    digitalWrite(12,HIGH);delay(25);
+    digitalWrite(17,HIGH);delay(1000);
+    digitalWrite(17,LOW);delay(25);
+    digitalWrite(12,LOW);delay(25);
+    digitalWrite(37,LOW);delay(25);
+    digitalWrite(16,LOW);delay(25);
+    digitalWrite(11,LOW);delay(25);
+    digitalWrite(36,LOW);delay(25);
+    digitalWrite(15,LOW);delay(25);
+    digitalWrite(10,LOW);delay(25);
+    digitalWrite(35,LOW);delay(25);
+    digitalWrite(14,LOW);delay(25);
+    digitalWrite(21,LOW);delay(25);
+    digitalWrite(6,LOW);delay(25);
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  while (! Serial);
+  encoder.setPosition(0 / ROTARYSTEPS); 
+  pullDownAllPins();
+  turnOffAllLights();
+}
+
+void loop() {
+  readEncoder();
+  
+  // if we have a new position 
+  if (lastNumber != currentNumber) {
+    turnOffNumber(lastNumber);
+    turnOnNumber(currentNumber);    
+  } else {
+    // TODO find a way to to work in Kevin's blinky code
+    // first thought is if the position doesn't change after ~1 sec it goes into "blinky" mode
+    //blingMode();
+  }
+  
+
 } 
