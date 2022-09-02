85985644d81aa69d33e1f36663f7fca081371dc6
Patrick Burns <patrickburns@patricks-mbp.lan>
2022-09-02 11:26:14 -0500

    encoder turns the lights

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,142 +1,170 @@
-#include <HardwareSerial.h>
+#include <Arduino.h>
+#include <RotaryEncoder.h>
 
-const int LEDB = 3;
-const int LEDS1 = 34;
-const int LEDI = 4;
-const int LEDD = 33;
-const int LEDE = 5;
-const int LEDS2 = 26;
+const int LED_B = 3;
+const int LED_S1 = 34;
+const int LED_I = 4;
+const int LED_D = 33;
+const int LED_E = 5;
+const int LED_S2 = 26;
 
-const int LED0 = 6;
-const int LED1 = 21;
-const int LED2 = 14;
-const int LED3 = 35;
-const int LED4 = 10;
-const int LED5 = 15;
-const int LED6 = 36;
-const int LED7 = 11;
-const int LED8 = 16;
-const int LED9 = 37;
-const int LED10 = 12;
-const int LED11 = 17;
+const int LED_0 = 6;
+const int LED_1 = 21;
+const int LED_2 = 14;
+const int LED_3 = 35;
+const int LED_4 = 10;
+const int LED_5 = 15;
+const int LED_6 = 36;
+const int LED_7 = 11;
+const int LED_8 = 16;
+const int LED_9 = 37;
+const int LED_10 = 12;
+const int LED_11 = 17;
 
-void setup() {
+// Example for Arduino UNO with input signals on pin 2 and 3
+#define PIN_IN1 13
+#define PIN_IN2 38
+
+
+#define ROTARYSTEPS 1
+#define ROTARYMIN 0
+#define ROTARYMAX 11
+
+// Setup a RotaryEncoder with 4 steps per latch for the 2 signal input pins:
+// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
+
+// Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+
+// Last known rotary position.
+int lastPos = -1;
+
+void setup()
+{
   Serial.begin(115200);
-  Serial.println("Setup");
+  while (! Serial);
+  Serial.println("LimitedRotator example for the RotaryEncoder library.");
+  encoder.setPosition(0 / ROTARYSTEPS); 
+
 
   // Setup LEDS
-  pinMode(LEDB, OUTPUT); 
-  pinMode(LEDS1, OUTPUT); 
-  pinMode(LEDI, OUTPUT); 
-  pinMode(LEDD, OUTPUT); 
-  pinMode(LEDE, OUTPUT); 
-  // pinMode(LEDS2, OUTPUT); 
-  pinMode(LED0, OUTPUT); 
-  pinMode(LED1, OUTPUT); 
-  pinMode(LED2, OUTPUT); 
-  pinMode(LED3, OUTPUT); 
-  pinMode(LED4, OUTPUT); 
-  pinMode(LED5, OUTPUT); 
-  pinMode(LED6, OUTPUT); 
-  pinMode(LED7, OUTPUT); 
-  pinMode(LED8, OUTPUT); 
-  pinMode(LED9, OUTPUT); 
-  pinMode(LED10, OUTPUT); 
-  pinMode(LED11, OUTPUT); 
+  pinMode(LED_B, OUTPUT); 
+  pinMode(LED_S1, OUTPUT); 
+  pinMode(LED_I, OUTPUT); 
+  pinMode(LED_D, OUTPUT); 
+  pinMode(LED_E, OUTPUT); 
+  pinMode(LED_S2, OUTPUT); 
+  pinMode(LED_0, OUTPUT); 
+  pinMode(LED_1, OUTPUT); 
+  pinMode(LED_2, OUTPUT); 
+  pinMode(LED_3, OUTPUT); 
+  pinMode(LED_4, OUTPUT); 
+  pinMode(LED_5, OUTPUT); 
+  pinMode(LED_6, OUTPUT); 
+  pinMode(LED_7, OUTPUT); 
+  pinMode(LED_8, OUTPUT); 
+  pinMode(LED_9, OUTPUT); 
+  pinMode(LED_10, OUTPUT); 
+  pinMode(LED_11, OUTPUT); 
 
-  digitalWrite(LEDB, LOW);   
-  digitalWrite(LEDS1, LOW);   
-  digitalWrite(LEDI, LOW);   
-  digitalWrite(LEDD, LOW);   
-  digitalWrite(LEDE, LOW);   
-  // digitalWrite(LEDS2, LOW); 
-  digitalWrite(LED0, LOW);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
-  digitalWrite(LED3, LOW);
-  digitalWrite(LED4, LOW);
-  digitalWrite(LED5, LOW);
-  digitalWrite(LED6, LOW);
-  digitalWrite(LED7, LOW);
-  digitalWrite(LED8, LOW);
-  digitalWrite(LED9, LOW);
-  digitalWrite(LED10, LOW);
-  digitalWrite(LED11, LOW);
-  
+  digitalWrite(LED_B, LOW);   
+  digitalWrite(LED_S1, LOW);   
+  digitalWrite(LED_I, LOW);   
+  digitalWrite(LED_D, LOW);   
+  digitalWrite(LED_E, LOW);   
+  digitalWrite(LED_S2, LOW); 
+  digitalWrite(LED_0, HIGH);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_4, LOW);
+  digitalWrite(LED_5, LOW);
+  digitalWrite(LED_6, LOW);
+  digitalWrite(LED_7, LOW);
+  digitalWrite(LED_8, LOW);
+  digitalWrite(LED_9, LOW);
+  digitalWrite(LED_10, LOW);
+  digitalWrite(LED_11, LOW);  
 }
 
-void loop() {    
-  digitalWrite(LEDB, HIGH);  
-  delay(50);
-  digitalWrite(LEDB, LOW);   
-    
-  digitalWrite(LEDS1, HIGH);  
-  delay(50);
-  digitalWrite(LEDS1, LOW);   
-  
-  digitalWrite(LEDI, HIGH);  
-  delay(50);
-  digitalWrite(LEDI, LOW);   
+void turnOnNumber(int numeral) {
+  digitalWrite(LED_0, LOW);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  digitalWrite(LED_3, LOW);
+  digitalWrite(LED_4, LOW);
+  digitalWrite(LED_5, LOW);
+  digitalWrite(LED_6, LOW);
+  digitalWrite(LED_7, LOW);
+  digitalWrite(LED_8, LOW);
+  digitalWrite(LED_9, LOW);
+  digitalWrite(LED_10, LOW);
+  digitalWrite(LED_11, LOW); 
 
-  digitalWrite(LEDD, HIGH);  
-  delay(50);
-  digitalWrite(LEDD, LOW);
-
-  digitalWrite(LEDE, HIGH);  
-  delay(50);
-  digitalWrite(LEDE, LOW);   
-
-  // digitalWrite(LEDS2, HIGH);  
-  // delay(200);
-  // digitalWrite(LEDS2, LOW);  
-
-  digitalWrite(LED0, HIGH);  
-  delay(50);
-  digitalWrite(LED0, LOW);   
-
-
-  digitalWrite(LED1, HIGH);  
-  delay(50);
-  digitalWrite(LED1, LOW);   
-
-  digitalWrite(LED2, HIGH);  
-  delay(50);
-  digitalWrite(LED2, LOW);   
-
-  digitalWrite(LED3, HIGH);  
-  delay(50);
-  digitalWrite(LED3, LOW);   
-
-  digitalWrite(LED4, HIGH);  
-  delay(50);
-  digitalWrite(LED4, LOW);   
-
-  digitalWrite(LED5, HIGH);  
-  delay(50);
-  digitalWrite(LED5, LOW);   
-
-
-  digitalWrite(LED6, HIGH);  
-  delay(50);
-  digitalWrite(LED6, LOW);   
-
-  digitalWrite(LED7, HIGH);  
-  delay(50);
-  digitalWrite(LED7, LOW);   
-
-  digitalWrite(LED8, HIGH);  
-  delay(50);
-  digitalWrite(LED8, LOW);   
-
-  digitalWrite(LED9, HIGH);  
-  delay(50);
-  digitalWrite(LED9, LOW);   
-
-  digitalWrite(LED10, HIGH);  
-  delay(50);
-  digitalWrite(LED10, LOW);   
-
-  digitalWrite(LED11, HIGH);  
-  delay(50);
-  digitalWrite(LED11, LOW);  
+    switch (numeral) {
+    case 0:
+        digitalWrite(LED_0, HIGH);
+        break;
+    case 1:
+        digitalWrite(LED_1, HIGH);
+        break;
+    case 2:
+        digitalWrite(LED_2, HIGH);
+        break;
+    case 3:
+        digitalWrite(LED_3, HIGH);
+        break;
+    case 4:
+        digitalWrite(LED_4, HIGH);
+        break;
+    case 5:
+        digitalWrite(LED_5, HIGH);
+        break;
+    case 6:
+        digitalWrite(LED_6, HIGH);
+        break;
+    case 7:
+        digitalWrite(LED_7, HIGH);
+        break;
+    case 8:
+        digitalWrite(LED_8, HIGH);
+        break;
+    case 9:
+        digitalWrite(LED_9, HIGH);
+        break;
+    case 10:
+        digitalWrite(LED_10, HIGH);
+        break;
+    case 11:
+        digitalWrite(LED_11, HIGH);
+        break;
+    default:
+        break;
+    }
 }
+
+
+// Read the current position of the encoder and print out when changed.
+void loop()
+{
+  encoder.tick();
+
+  // get the current physical position and calc the logical position
+  int newPos = encoder.getPosition() * ROTARYSTEPS;
+
+  if (newPos < ROTARYMIN) {
+    encoder.setPosition(ROTARYMAX / ROTARYSTEPS);
+    newPos = ROTARYMAX;
+
+  } else if (newPos > ROTARYMAX) {
+    encoder.setPosition(ROTARYMIN / ROTARYSTEPS);
+    newPos = ROTARYMIN;
+  } 
+
+  if (lastPos != newPos) {
+    turnOnNumber(newPos);
+    Serial.print(newPos);
+    Serial.println();
+    lastPos = newPos;
+  }
+} 
