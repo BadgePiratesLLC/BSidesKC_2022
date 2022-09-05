532c71be6c4b7bfce0a6aaba9efcabcbf1e67690
Patrick Ecord <patrick.ecord@riskiq.net>
2022-09-05 12:42:54 -0500

    add a constants and pins file

---
+++ code/src/constants.h
@@ -0,0 +1,12 @@
+#define BLING_MODE_TIMEOUT_INTERVAL 6 // 500ms * 2  = ~one seconds
+#define DEBUG false
+#define INTERVAL 500 // ms
+#define INVERT_DIR false
+#define NUM_BLING 2
+#define OFF_TIME 25  // milliseconds of off-time
+#define ON_TIME 25   // milliseconds of on-time
+#define ROTARY_MAX 11
+#define ROTARY_MIN 0
+#define ROTARY_STEPS 1
+#define ROTARY_SWITCH 7
+#define VERBOSE false
