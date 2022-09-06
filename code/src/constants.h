2df160fbbf047e0566c186073b95a7ac3eb903b4
Patrick Burns <burnsoft@gmail.com>
2022-09-06 07:37:04 -0500

    move rotary switch over to pin.h

--- code/src/constants.h
+++ code/src/constants.h
@@ -3,10 +3,9 @@
 #define INTERVAL 500 // ms
 #define INVERT_DIR false
 #define NUM_BLING 2
-#define OFF_TIME 25  // milliseconds of off-time
-#define ON_TIME 25   // milliseconds of on-time
+#define OFF_TIME 25 // milliseconds of off-time
+#define ON_TIME 25  // milliseconds of on-time
 #define ROTARY_MAX 11
 #define ROTARY_MIN 0
 #define ROTARY_STEPS 1
-#define ROTARY_SWITCH 7
 #define VERBOSE false
